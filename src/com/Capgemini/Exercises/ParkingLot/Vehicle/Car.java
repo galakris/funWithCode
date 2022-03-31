f9d13bf47f3bac110fec3d65b3a2459d2b0a856e
Krzysztof Gałuszka <krzysztof.galuszka@gmail.com>
2022-03-31 14:02:17 +0200

    Init commit

---
+++ src/com/Capgemini/Exercises/ParkingLot/Vehicle/Car.java
@@ -0,0 +1,8 @@
+package com.Capgemini.Exercises.ParkingLot.Vehicle;
+
+public class Car extends Vehicle {
+
+    public Car(String brand, String model, VehicleType vehicleType) {
+        super(brand, model, vehicleType);
+    }
+}
